89d6c8a33401ea4f6deb7c4a4d1d128f5a8b375a
Ibrahima Diarra <idiarra@gmail.com>
2015-03-06 17:38:53 -0600

    Binary search ST implementation

--- javascriptPlayground/src/search/BinarySearchST.js
+++ javascriptPlayground/src/search/BinarySearchST.js
@@ -2,21 +2,46 @@
  * Symbol Table based on binary search on ordered array.
  */
 BinarySearchST = function() {
-	
-	var keys = [], values = [];
-	
+
+	var keys = [], values = [], me = this;
+
 	this.put = function(k, v) {
-		print(keys);
+		var i = me.rank(k);
+		if(i < keys.length && keys[i] === k)
+			values[i] = v;
+		for(var x = keys.length; x > i; x--){
+			keys[x] = keys[x-1];
+			values[x] = values[x-1];
+		}
+		keys[i] = k; 
+		values[i] = v;
 	};
-	
-	this.get = function(k){
-		
+
+	this.get = function(k) {
+		var i = me.rank(k);
+		if(i < keys.length && k === keys[i])
+			return values[i];
+		return null;
 	};
-	
-	this.rank = function(k){
-		
+
+	this.rank = function(k) {
+		var lo = 0, hi = keys.length, mid, midTemp;
+		while (lo <= hi) {
+			midTemp = (hi-lo)/2;
+			mid =  (lo + midTemp) | 0;
+			if (k < keys[mid])
+				hi = mid - 1;
+			else if (k > keys[mid])
+				lo = mid + 1;
+			else
+				return mid;
+		}
+		return lo;
 	};
 };
 
 var st = new BinarySearchST();
-st.put();
+st.put("mango", "mango".length);
+st.put("apple", "apple".length);
+st.put("orange", "orange".length);
+print("mango -> " + st.get("mango"));
